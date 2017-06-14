3318a56eb9e75a363c97fcd17fbff1d15771b4f3
破娃酱 <mmgac001@gmail.com>
2017-06-14 15:52:46 +0800

    refine ServerLogForm

--- shadowsocks-csharp/View/ServerLogForm.cs
+++ shadowsocks-csharp/View/ServerLogForm.cs
@@ -322,9 +322,6 @@ namespace Shadowsocks.View
                     ++list_index)
                 {
                     lastRefreshIndex = list_index + 1;
-                    if (list_index < displayBeginIndex || list_index >= displayEndIndex)
-                        continue;
-                    ++rowChangeCnt;
 
                     DataGridViewCell id_cell = ServerDataGrid[0, list_index];
                     int id = (int)id_cell.Value;
@@ -334,6 +331,10 @@ namespace Shadowsocks.View
                     rowChange = false;
                     for (int curcol = 0; curcol < ServerDataGrid.Columns.Count; ++curcol)
                     {
+                        if (!firstDispley
+                            && (ServerDataGrid.SortedColumn == null || ServerDataGrid.SortedColumn.Index != curcol)
+                            && (list_index < displayBeginIndex || list_index >= displayEndIndex))
+                            continue;
                         DataGridViewCell cell = ServerDataGrid[curcol, list_index];
                         string columnName = ServerDataGrid.Columns[curcol].Name;
                         // Server
@@ -613,7 +614,7 @@ namespace Shadowsocks.View
                             }
                         }
                     }
-                    if (rowChange)
+                    if (rowChange && list_index >= displayBeginIndex && list_index < displayEndIndex)
                         rowChangeCnt++;
                 }
             }
@@ -1008,6 +1009,7 @@ namespace Shadowsocks.View
         private long Str2Long(String str)
         {
             if (str == "-") return -1;
+            //if (String.IsNullOrEmpty(str)) return -1;
             if (str.LastIndexOf('K') > 0)
             {
                 Double ret = Convert.ToDouble(str.Substring(0, str.LastIndexOf('K')));
