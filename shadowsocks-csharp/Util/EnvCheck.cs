0d6e6b126a9e4884c83caa41ea7789a8715dd127
Aspirin Geyer <aspirin.geyer@yandex.com>
2018-02-11 03:51:02 +0800

    Capture one exception.

--- shadowsocks-csharp/Util/EnvCheck.cs
+++ shadowsocks-csharp/Util/EnvCheck.cs
@@ -8,13 +8,13 @@ namespace Shadowsocks.Util
     {
         // According to https://docs.microsoft.com/en-us/dotnet/framework/migration-guide/how-to-determine-which-versions-are-installed
         // Hard code the path in Registry.
-        private static string dotNet45Registry = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full";
+        private static string dotNet45Registry = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\x1";
 
         public static bool CheckDotNet45()
         {
             Int32 installed = Convert.ToInt32(Registry.GetValue(dotNet45Registry, "Release", 0));
             if (0 == installed)
-                throw new SystemException(String.Format("Cannot find .NET version from registry path: {0}.", dotNet45Registry));
+                return false;
 
             if (378389 <= installed)
                 return true;
