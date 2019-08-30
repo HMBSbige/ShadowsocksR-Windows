838113dbf7e9a456c05706cf019752e577e50420
Bruce Wayne <admin@bige0.com>
2019-08-30 18:41:07 +0800

    Build net48 in appveyor

--- publish.cmd
+++ publish.cmd
@@ -3,8 +3,8 @@
 echo package .NET Core
 7z a ShadowsocksR-netcore.zip %~dp0\shadowsocks-csharp\bin\Release\netcoreapp3.0\publish\ShadowsocksR.exe %~dp0\shadowsocks-csharp\bin\Release\netcoreapp3.0\publish\ShadowsocksR.dll %~dp0\shadowsocks-csharp\bin\Release\netcoreapp3.0\publish\ShadowsocksR.runtimeconfig.json
 
-REM echo package .NET Framework
-REM 7z a ShadowsocksR-net48.zip %~dp0\shadowsocks-csharp\bin\Release\net48\ShadowsocksR.exe
+echo package .NET Framework
+7z a ShadowsocksR-net48.zip %~dp0\shadowsocks-csharp\bin\Release\net48\ShadowsocksR.exe
 
 REM echo package .NET Core SelfContained x86
 REM 7z a ShadowsocksR-netcore-win32.zip %~dp0\shadowsocks-csharp\bin\Release\netcoreapp3.0\win-x86\publish\ShadowsocksR.exe
