3331feebad4eaa2264e326ebd16fbc7502eee00b
Bruce Wayne <admin@bige0.com>
2019-09-24 18:24:30 +0800

    Bump version number to 5.1.1

--- shadowsocks-csharp/Controller/Service/UpdateChecker.cs
+++ shadowsocks-csharp/Controller/Service/UpdateChecker.cs
@@ -25,7 +25,7 @@ namespace Shadowsocks.Controller.Service
 
         public const string Name = @"ShadowsocksR";
         public const string Copyright = @"Copyright © HMBSbige 2019 & BreakWa11 2017. Fork from Shadowsocks by clowwindy";
-        public const string Version = @"5.1.0.1";
+        public const string Version = @"5.1.1";
 
         public const string FullVersion = Version +
 #if IsDotNetCore
