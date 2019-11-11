ec44e650667307421aacac54062b437db759271f
谭九鼎 <109224573@qq.com>
2019-11-11 17:06:05 +0800

    自动restore

--- build.cmd
+++ build.cmd
@@ -10,20 +10,16 @@ REM dotnet publish -c Release -f netcoreapp3.0 -r win-x64 --self-contained
 cd shadowsocks-csharp
 
 echo Building .NET Core
-msbuild -v:m -t:Restore -p:Configuration=Release -p:TargetFramework=netcoreapp3.0 || goto :error
-msbuild -v:m -t:Publish -p:Configuration=Release -p:TargetFramework=netcoreapp3.0 || goto :error
+msbuild -v:m -r -t:Publish -p:Configuration=Release -p:TargetFramework=netcoreapp3.0 || goto :error
 
 echo Building .NET Framework x86 and x64
-msbuild -v:m -t:Restore -p:Configuration=Release -p:TargetFramework=net48 || goto :error
-msbuild -v:m -t:Build -p:Configuration=Release -p:TargetFramework=net48 || goto :error
+msbuild -v:m -r -t:Build -p:Configuration=Release -p:TargetFramework=net48 || goto :error
 
 echo Building .NET Core SelfContained x86
-msbuild -v:m -t:Restore -p:Configuration=Release -p:TargetFramework=netcoreapp3.0 -p:RuntimeIdentifier=win-x86 -p:SelfContained=True -p:PublishSingleFile=true|| goto :error
-msbuild -v:m -t:Publish -p:Configuration=Release -p:TargetFramework=netcoreapp3.0 -p:RuntimeIdentifier=win-x86 -p:SelfContained=True -p:PublishSingleFile=true|| goto :error
+msbuild -v:m -r -t:Publish -p:Configuration=Release -p:TargetFramework=netcoreapp3.0 -p:RuntimeIdentifier=win-x86 -p:SelfContained=True -p:PublishSingleFile=true|| goto :error
 
 echo Building .NET Core SelfContained x64
-msbuild -v:m -t:Restore -p:Configuration=Release -p:TargetFramework=netcoreapp3.0 -p:RuntimeIdentifier=win-x64 -p:SelfContained=True -p:PublishSingleFile=true|| goto :error
-msbuild -v:m -t:Publish -p:Configuration=Release -p:TargetFramework=netcoreapp3.0 -p:RuntimeIdentifier=win-x64 -p:SelfContained=True -p:PublishSingleFile=true|| goto :error
+msbuild -v:m -r -t:Publish -p:Configuration=Release -p:TargetFramework=netcoreapp3.0 -p:RuntimeIdentifier=win-x64 -p:SelfContained=True -p:PublishSingleFile=true|| goto :error
 
 cd..
 goto :EOF
