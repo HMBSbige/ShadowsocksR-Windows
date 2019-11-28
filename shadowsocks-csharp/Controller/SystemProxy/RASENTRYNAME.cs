0dd74e22d41c51182069626cdbef2dfc5ca3f56b
Bruce Wayne <admin@bige0.com>
2019-11-29 00:55:18 +0800

    Fix #156

---
+++ shadowsocks-csharp/Controller/SystemProxy/RASENTRYNAME.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Shadowsocks.Controller.SystemProxy
+{
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    internal struct RASENTRYNAME
+    {
+        public int Size;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = NativeMethods.RasMaxEntryName + 1)]
+        public string EntryName;
+
+        public int Flags;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = NativeMethods.MAX_PATH + 1)]
+        public string Phonebook;
+    }
+}
