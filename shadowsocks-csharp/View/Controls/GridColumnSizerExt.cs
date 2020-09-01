0911d971091fef269ec964770cf06142d0bf0334
Bruce Wayne <admin@bige0.com>
2020-09-01 09:26:29 +0800

    Fix columns' autosize

---
+++ shadowsocks-csharp/View/Controls/GridColumnSizerExt.cs
@@ -0,0 +1,26 @@
+﻿using Syncfusion.UI.Xaml.Grid;
+using System.Linq;
+using System.Windows;
+
+namespace Shadowsocks.View.Controls
+{
+    public class GridColumnSizerExt : GridColumnSizer
+    {
+        public GridColumnSizerExt(SfDataGrid dataGrid) : base(dataGrid) { }
+
+        protected override double CalculateCellWidth(GridColumn column, bool setWidth = true)
+        {
+            var length = base.CalculateCellWidth(column, setWidth);
+
+            if (column is GridTextColumn)
+            {
+                var clientSize = new Size(double.MaxValue, DataGrid.RowHeight);
+                length = DataGrid.View.Records.Where(recordEntry => recordEntry?.Data != null)
+                    .Select(recordEntry => GetDisplayText(column, recordEntry.Data)).Select(text =>
+                        MeasureText(clientSize, text, column, null, GridQueryBounds.Width).Width).Prepend(length).Max();
+            }
+
+            return length;
+        }
+    }
+}
