88d75477aa4246e6c6696fca1a6ccb9fad7937e0
Bruce Wayne <admin@bige0.com>
2021-07-29 09:49:39 +0800

    perf: Faster CRC32

--- shadowsocks-csharp/Util/CRC.cs
+++ shadowsocks-csharp/Util/CRC.cs
@@ -1,84 +1,36 @@
+using CryptoBase.Digests;
+using System;
+using System.Buffers.Binary;
+
 namespace Shadowsocks.Util
 {
-    class CRC32
+    public static class CRC32
     {
-        protected static ulong[] Crc32Table = CreateCRC32Table();
-        //生成CRC32码表  
-        public static ulong[] CreateCRC32Table()
+        public static ulong CalcCRC32(byte[] input, int len)
         {
-            ulong Crc;
-            Crc32Table = new ulong[256];
-            int i, j;
-            for (i = 0; i < 256; i++)
-            {
-                Crc = (ulong)i;
-                for (j = 8; j > 0; j--)
-                {
-                    if ((Crc & 1) == 1)
-                    {
-                        Crc = (Crc >> 1) ^ 0xEDB88320;
-                    }
-                    else
-                    {
-                        Crc >>= 1;
-                    }
-                }
-                Crc32Table[i] = Crc;
-            }
-            return Crc32Table;
-        }
-
-        //获取字符串的CRC32校验值
-        public static ulong CalcCRC32(byte[] input, int len, ulong value = 0xffffffff)
-        {
-            return CalcCRC32(input, 0, len, value);
-        }
-        public static ulong CalcCRC32(byte[] input, int index, int len, ulong value = 0xffffffff)
-        {
-            var buffer = input;
-            for (var i = index; i < len; i++)
-            {
-                value = (value >> 8) ^ Crc32Table[(value & 0xFF) ^ buffer[i]];
-            }
-            return value ^ 0xffffffff;
+            using var hash = DigestUtils.Create(DigestType.Crc32);
+            var t = new byte[hash.Length];
+            hash.UpdateFinal(input.AsSpan(0, len), t);
+            return BinaryPrimitives.ReadUInt32BigEndian(t);
         }
 
         public static void SetCRC32(byte[] buffer)
         {
-            SetCRC32(buffer, 0, buffer.Length);
-        }
-
-        public static void SetCRC32(byte[] buffer, int length)
-        {
-            SetCRC32(buffer, 0, length);
-        }
-        public static void SetCRC32(byte[] buffer, int index, int length)
-        {
-            var crc = ~CalcCRC32(buffer, index, length - 4);
-            buffer[length - 1] = (byte)(crc >> 24);
-            buffer[length - 2] = (byte)(crc >> 16);
-            buffer[length - 3] = (byte)(crc >> 8);
-            buffer[length - 4] = (byte)crc;
-        }
-
-        public static bool CheckCRC32(byte[] buffer, int length)
-        {
-            var crc = CalcCRC32(buffer, length);
-            if (crc != 0xffffffffu)
-            {
-                return false;
-            }
-
-            return true;
+            using var hash = DigestUtils.Create(DigestType.Crc32);
+            var t = new byte[hash.Length];
+            hash.UpdateFinal(buffer.AsSpan(0, buffer.Length - 4), t);
+            var x = ~BinaryPrimitives.ReadUInt32BigEndian(t);
+            BinaryPrimitives.WriteUInt32LittleEndian(buffer.AsSpan(buffer.Length - 4), x);
         }
     }
-    class Adler32
+
+    public static class Adler32
     {
         public static ulong CalcAdler32(byte[] input, int len)
         {
             ulong a = 1;
             ulong b = 0;
-            for (var i = 0; i < len; i++)
+            for (var i = 0; i < len; ++i)
             {
                 a += input[i];
                 b += a;
@@ -91,15 +43,8 @@ namespace Shadowsocks.Util
         public static bool CheckAdler32(byte[] input, int len)
         {
             var adler32 = CalcAdler32(input, len - 4);
-            var checksum = (input[len - 1] << 24) | (input[len - 2] << 16) | (input[len - 3] << 8) | input[len - 4];
-            return (int)adler32 == checksum;
-        }
-
-        public static bool CheckAdler32(byte[] input, int len, uint xor)
-        {
-            var adler32 = CalcAdler32(input, len - 4) ^ xor;
-            var checksum = (input[len - 1] << 24) | (input[len - 2] << 16) | (input[len - 3] << 8) | input[len - 4];
-            return (int)adler32 == checksum;
+            var checksum = BinaryPrimitives.ReadUInt32LittleEndian(input.AsSpan(len - 4));
+            return (uint)adler32 == checksum;
         }
     }
 }
