e478572d12bbb33d9114b2f5df03437458f9439c
Bruce Wayne <admin@bige0.com>
2022-01-15 00:09:10 +0800

    dep: Update deps

--- shadowsocks-csharp/Util/QrCodeUtils.cs
+++ shadowsocks-csharp/Util/QrCodeUtils.cs
@@ -7,83 +7,83 @@ using ZXing;
 using ZXing.Common;
 using ZXing.QrCode;
 using ZXing.QrCode.Internal;
+using ZXing.Windows.Compatibility;
 using Brush = System.Drawing.Brush;
 using Color = System.Drawing.Color;
 
-namespace Shadowsocks.Util
+namespace Shadowsocks.Util;
+
+public static class QrCodeUtils
 {
-    public static class QrCodeUtils
+    private static BitmapImage ToBitmapImage(Image src)
     {
-        private static BitmapImage ToBitmapImage(Image src)
-        {
-            var ms = new MemoryStream();
-            src.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
-            var image = new BitmapImage();
-            image.BeginInit();
-            ms.Seek(0, SeekOrigin.Begin);
-            image.StreamSource = ms;
-            image.EndInit();
-            return image;
-        }
+        var ms = new MemoryStream();
+        src.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+        var image = new BitmapImage();
+        image.BeginInit();
+        ms.Seek(0, SeekOrigin.Begin);
+        image.StreamSource = ms;
+        image.EndInit();
+        return image;
+    }
 
-        public static ImageSource GenQrCode(string content, int width, int height)
+    public static ImageSource GenQrCode(string content, int width, int height)
+    {
+        var options = new QrCodeEncodingOptions
         {
-            var options = new QrCodeEncodingOptions
-            {
-                DisableECI = true,
-                CharacterSet = @"UTF-8",
-                Width = width,
-                Height = height,
-                Margin = 0,
-                ErrorCorrection = ErrorCorrectionLevel.H
-            };
-            var write = new BarcodeWriter
-            {
-                Format = BarcodeFormat.QR_CODE,
-                Options = options
-            };
-            var bitmap = write.Write(content);
-            return ToBitmapImage(bitmap);
-        }
-
-        public static ImageSource GenQrCode2(string str, int height)
+            DisableECI = true,
+            CharacterSet = @"UTF-8",
+            Width = width,
+            Height = height,
+            Margin = 0,
+            ErrorCorrection = ErrorCorrectionLevel.H
+        };
+        var write = new BarcodeWriter
         {
-            var options = new QrCodeEncodingOptions
-            {
-                DisableECI = true,
-                CharacterSet = @"UTF-8",
-                Margin = 0
-            };
-            var code = Encoder.encode(str, ErrorCorrectionLevel.H, options.Hints);
-            var m = code.Matrix;
-            var blockSize = Math.Max(height / (m.Height + 1), 1);
+            Format = BarcodeFormat.QR_CODE,
+            Options = options
+        };
+        var bitmap = write.Write(content);
+        return ToBitmapImage(bitmap);
+    }
 
-            var drawArea = new Bitmap(m.Width * blockSize, m.Height * blockSize);
-            using (var g = Graphics.FromImage(drawArea))
+    public static ImageSource GenQrCode2(string str, int height)
+    {
+        var options = new QrCodeEncodingOptions
+        {
+            DisableECI = true,
+            CharacterSet = @"UTF-8",
+            Margin = 0
+        };
+        var code = Encoder.encode(str, ErrorCorrectionLevel.H, options.Hints);
+        var m = code.Matrix;
+        var blockSize = Math.Max(height / (m.Height + 1), 1);
+
+        var drawArea = new Bitmap(m.Width * blockSize, m.Height * blockSize);
+        using (var g = Graphics.FromImage(drawArea))
+        {
+            g.Clear(Color.White);
+            using Brush b = new SolidBrush(Color.Black);
+            for (var row = 0; row < m.Width; ++row)
             {
-                g.Clear(Color.White);
-                using Brush b = new SolidBrush(Color.Black);
-                for (var row = 0; row < m.Width; ++row)
+                for (var col = 0; col < m.Height; ++col)
                 {
-                    for (var col = 0; col < m.Height; ++col)
+                    if (m[row, col] != 0)
                     {
-                        if (m[row, col] != 0)
-                        {
-                            g.FillRectangle(b, blockSize * row, blockSize * col, blockSize, blockSize);
-                        }
+                        g.FillRectangle(b, blockSize * row, blockSize * col, blockSize, blockSize);
                     }
                 }
             }
-
-            return ToBitmapImage(drawArea);
         }
 
-        public static Result ScanBitmap(Bitmap target)
-        {
-            var source = new BitmapLuminanceSource(target);
-            var bitmap = new BinaryBitmap(new HybridBinarizer(source));
-            var reader = new QRCodeReader();
-            return reader.decode(bitmap);
-        }
+        return ToBitmapImage(drawArea);
+    }
+
+    public static Result ScanBitmap(Bitmap target)
+    {
+        var source = new BitmapLuminanceSource(target);
+        var bitmap = new BinaryBitmap(new HybridBinarizer(source));
+        var reader = new QRCodeReader();
+        return reader.decode(bitmap);
     }
 }
